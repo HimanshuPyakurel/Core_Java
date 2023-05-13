14b349798311f34e6b38b101b6b8e67fe6167159
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-05-13 11:42:42 +0545

    Add: Method Documentation

---
+++ Core_Java/src/methods/MethodDocs.java
@@ -0,0 +1,38 @@
+package methods;
+
+public class MethodDocs {
+	
+	/*
+	 * =================== Method ===================
+	 *  # Code re-usability
+	 *  # Lines or block of code which is used to perform some specific
+	 *    task in the program is known as method
+	 *    
+	 *   # syntax:
+	 *      
+	 *       return_type method_name(arg1,arg2,...,argn){     
+	 *       //logics
+	 *       }
+	 *       
+	 *   # Return Type :
+	 *     a> void     : no value(result) return from method
+	 *     b> non-void : value(result) return from method
+	 * 
+	 */
+	
+	void read() {
+		
+		area(23, 34);
+	}
+	
+	void area(int l, int b) {
+		
+	}
+	
+	void go() {
+		
+		read();
+	}
+	
+	
+}
