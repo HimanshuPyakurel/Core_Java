9c6785f128fc2fd801c61cbb8550325fcba3e43a
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-05-15 15:15:45 +0545

    Add: Array Documentation with syntax

---
+++ Core_Java/src/Array/ArrayDocs.java
@@ -0,0 +1,34 @@
+package Array;
+
+public class ArrayDocs {
+	
+	/*
+	 * 	================== Array =======================================
+	 * 
+	 *	# use to store and processing homogeneous data with fixed size.
+	 *  # collection/group of multiple values with same data type.
+	 *  # data stored on the basis of indices(index starts from 0)
+	 *  # contains duplicate data
+	 *  # random access
+	 *  # contiguous memory allocation
+	 *  # array implicitly called as a object in java
+	 *  
+	 *  
+	 *  syntax:
+	 *  
+	 *     data_type array_name[] = new data_type[Size]; //instantiation(object creation)
+	 *     
+	 *     eg:
+	 *     
+	 *     	float bookPrice[] = new float[50];
+	 *      int []age = new int[500];
+	 *      char[] vs = new char[5];
+	 *      int salary[] = new int[500];
+	 *      
+	 *     // Employee emps[] = new Employee[200];
+	 * 
+	 */
+
+	
+	
+}
