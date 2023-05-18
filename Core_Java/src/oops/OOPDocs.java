d96ec2fa80aa9565866ebf1de5c75b8f6839bcfa
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-05-18 10:02:32 +0545

    Add: OOP Documentation

---
+++ Core_Java/src/oops/OOPDocs.java
@@ -0,0 +1,57 @@
+package oops;
+
+public class OOPDocs {
+
+	/*
+	 * ================== OOP =============--------------------
+	 *   
+	 *   1> Class and object
+	 *   2> Encapsulation
+	 *   3> Inheritance
+	 *   4> Polymorphism
+	 *   5> Abstraction
+	 *      a) Abstract class
+	 *      b) Interface
+	 *      
+	 *    * Cohesive(gathering on one) must be high and 
+	 *    * Coupling(dependency between package) must be low
+	 *      
+	 *   
+	 *  ====================== Class & Object ===================
+	 *      
+	 *    # Class :-
+	 *    -----------
+	 *      -> use to represent real world entities...(like car, jacket, pen,etc)
+	 *      -> class contain properties and methods.
+	 *      -> its a collection/group of object having similar behaviors.
+	 *      -> also called template/blue print of object.
+	 *      -> its a user defined and reference data type.
+	 *       
+	 *      syntax:
+	 *      
+	 *        class class_name{
+	 *        
+	 *       	 //Properties
+	 *        
+	 *        	// Methods
+	 *        
+	 *        }
+	 *        
+	 *        
+	 *    # Object/Instance :-
+	 *    --------------------
+	 *        -> implementation of class.
+	 *        -> instance of class.
+	 *        -> object contains instance variable and instance methods.
+	 *        -> object allocates memory when it created
+	 *        
+	 *        syntax:
+	 *        
+	 *            class_name object_name = new class_name();
+	 *                     
+	 *      
+	 */
+	
+	
+	
+}
