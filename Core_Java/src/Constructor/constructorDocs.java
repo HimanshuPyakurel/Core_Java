911120a09b9f7bc74e1733f4e9bbbfceff6ced8b
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-05-19 15:34:51 +0545

    Add: Constructor Documentation

---
+++ Core_Java/src/Constructor/constructorDocs.java
@@ -0,0 +1,42 @@
+package Constructor;
+
+public class constructorDocs {
+
+	
+	/*
+	 * ==================== Constructor ========================================
+	 * 
+	 *  # Use to create object in java.
+	 *  # special type of method which has same name as class name.
+	 *  # no return type but return value...
+	 *  # if there is no user defined constructor in class then java provides
+	 *    default constructor to create object.
+	 *  
+	 *  # Types :
+	 *  
+	 *    1> Default Constructor:
+	 *     -----------------------
+	 *    	-> use to create object and default value to it.
+	 *    
+	 *    	syntax:
+	 *         class_name(){
+	 *         
+	 *         //set default value to object
+	 *         }
+	 *    
+	 *    	
+	 *    2> Parameterized Constructor: 
+	 *      ---------------------------
+	 *      -> use to create object and set dynamic value to it.
+	 *      
+	 *       syntax:
+	 *         class_name(arg1,arg2,arg3,...,argn){
+	 *         
+	 *          //set dynamic values to object
+	 *         }
+	 * 
+	 */
+	
+	
+	
+}
