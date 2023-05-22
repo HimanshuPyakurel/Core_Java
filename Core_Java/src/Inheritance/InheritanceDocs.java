65cb2362d923bd3577e1afbc0caf2f2eb29f0010
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-05-22 08:57:11 +0545

    Add: Inheritance Documentation

---
+++ Core_Java/src/Inheritance/InheritanceDocs.java
@@ -0,0 +1,33 @@
+package Inheritance;
+
+public class InheritanceDocs {
+	/*
+	 * =================== Inheritance ======================================
+	 * 
+	 *   # code re-usability
+	 *   # process of accessing properties and methods of parent/super/base
+	 *     class to child/sub/derived class is known as inheritance
+	 *     
+	 *   # also called IS-A relationship
+	 *   # Its a generalization to specialization process. 
+	 *     eg: vehicle is generalization and Mercedes car is specialization term.
+	 *     
+	 *   # we can inherit static properties and method also
+	 *   # we cann't inherit private properties, method and constructors.
+	 *   # we cann't inherit final class.
+	 *   # we can inherit only one class at a time.
+	 *   # 'extends' keyword is used for inheritance.
+	 *   
+	 *   syntax: 
+	 *    
+	 *    class child_class_name  extends parent_class{
+	 *    
+	 *    // properties : parent + child
+	 *    
+	 *    // methods : parent + child 
+	 *    
+	 *    }
+	 *  
+	 */
+
+}
