1c1b69f1ace8b3f0572bd3d194db4714ca646d82
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-05-22 08:38:41 +0545

    Add: Encapsulation Documentation

---
+++ Core_Java/src/Encapsulation/EncapsulationDocs.java
@@ -0,0 +1,16 @@
+package Encapsulation;
+
+public class EncapsulationDocs {
+	/*
+	 * =================== Encapsulation =================
+	 *  # process of data hiding and binding.
+	 *  
+	 *  # To achieve encapsulation :
+	 *   a> declare properties of class as private (i.e. data hiding)
+	 *   b> create public getters and setters methods to get and set values to private
+	 *      to private variables(i.e. data binding)
+	 * 
+	 */
+	
+
+}
