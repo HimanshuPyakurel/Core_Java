38e8fbb33a1a9f4c6120afbd01593695580ab894
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-05-23 08:57:42 +0545

    Fix: Store and print data of 5 product Making array of object

--- Core_Java/src/ObjectWithArray/Product_Test.java
+++ Core_Java/src/ObjectWithArray/Product_Test.java
@@ -1,21 +1,34 @@
 package ObjectWithArray;
 
+import java.util.Scanner;
+
 public class Product_Test {
 	public static void main(String[] args) {
 	
 		// WAP to store and print data of 5 products
 		
 		Product[] products = new Product[5];
+		Scanner sc = new Scanner(System.in);
 		
 		//Store product in array
 		
-		for(int i=0;i<5;i++) {
+		for(int i=0; i<5; i++) {
 		
 			Product p = new Product();
-			p.setId(444);
-			p.setName("Mobile");
-			p.setPrice(4000);
-			p.setCompany("Samsung");
+			
+			System.out.println("Enter Id : ");
+			p.setId(sc.nextInt());
+			
+			System.out.println("Enter Product Name : ");
+			p.setName(sc.next());
+			
+			System.out.println("Enter Product Price : ");
+			p.setPrice(sc.nextInt());
+			
+			System.out.println("Enter Company Name : ");
+			p.setCompany(sc.next());
+			 
+			System.out.println(" ");
 			
 			products[i] = p;
 
