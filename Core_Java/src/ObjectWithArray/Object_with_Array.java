72ff46c1a2cc6a2347ba7207244a274fbcbc5f94
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-05-23 08:49:11 +0545

    Add: Object In Array

---
+++ Core_Java/src/ObjectWithArray/Object_with_Array.java
@@ -0,0 +1,33 @@
+package ObjectWithArray;
+
+public class Object_with_Array {
+
+	/*
+	 * ======================== Object With Array ======================
+	 * 
+	 *   syntax:
+	 *   	
+	 *    class_name array_name[] = new class_name[size];
+	 *    
+	 *    eg: 
+	 *    	
+	 *     int data[] = new int[40];
+	 *      data[0]=3212; //Integer Type
+	 *     
+	 *     Student s[] = new Student[200]; // array of object
+	 *    
+	 *     Student s1 = new Student();
+	 *     s1.setId(3421);
+	 *     .......
+	 *     .......
+	 *     s2.setId(3423);
+	 *     .......
+	 *     .......
+	 *     
+	 *     s[0] = s1;
+	 *     s[1] = s2;
+	 *     s[2] = s3;
+	 *    
+	 */
+	
+}
