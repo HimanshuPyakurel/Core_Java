d95f9f8c709477e16c96fd570744f40e8c52d9fb
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-05-24 12:13:56 +0545

    Add: Object with Array

---
+++ Core_Java/src/ObjectWithMethod/object_with_Method.java
@@ -0,0 +1,41 @@
+package ObjectWithMethod;
+
+public class object_with_Method {
+	/*
+	 * ===================== Object With Method ======================
+	 * 
+	 *   a> Object as parameter:
+	 *    
+	 *    Example :-
+	 *    
+	 *      Student : id,fn,ln,college,...,30th 
+	 *      
+	 *      void printStudent(Student s){
+	 *      
+	 *      } 
+	 *      
+	 *      Student std = new Student(); // create Object
+	 *      std.id = 34332;
+	 *      std.fn = "Himanshu";
+	 *      ..............
+	 *      .............
+	 *      
+	 *      printStudent(std); // Object as parameter
+	 *   
+	 *   
+	 *   
+	 *   b> Object as return Type:
+	 *   
+	 *   Example :-
+	 *   
+	 *    	Student getStudentData(){
+	 *    
+	 *    	//create Student Object
+	 *    	//set all value
+	 *    
+	 *   	 return object of student;
+	 *    
+	 *    	}
+	 */
+
+}
