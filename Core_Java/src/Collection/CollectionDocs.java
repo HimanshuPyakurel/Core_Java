2b9523167eef13be26c18b0e4384a0d88ad65f01
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-05-29 09:22:19 +0545

    Add : Collection Documentation (list, set and map)

---
+++ Core_Java/src/Collection/CollectionDocs.java
@@ -0,0 +1,27 @@
+package Collection;
+
+public class CollectionDocs {
+
+	/*
+	 *  =============== Collection Framework / Java Collection / DS ====================
+	 *  # Use to store and process dynamic multiple data/object
+	 *  
+	 *  1> List : dynamic array
+	 *      a> ArrayList : slow processing, better to use for data storage
+	 *      b> LinkedList : fast processing, better to use for data processing
+	 *  
+	 *  2> Set : collection of unique data.
+	 *      a> HastSet       : Unique data only 
+	 *      b> TreeSet       : unique + sorted data
+	 *      c> LinkedHashSet : unique + insertion in oder (show data as per the insertion)
+	 *      
+	 *  3> Map : data manage in key,value pair <k,v>
+	 *      a> HashMap       : <k,v> pair 
+	 *      b> TreeMap       : <k,v> + sorted by key
+	 *      c> LinkedHashMap : <k,v> + insertion in order as per key
+	 *      
+	 */
+	
+	
+	
+}
