b0d936e5280cd4b5473df8d210bf908e59341e9a
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-06-01 22:52:31 +0545

    test: Implementation class with jdbc (MySql database)

--- CoreJavaDemo/src/com/service/ProductServiceImpl.java
+++ CoreJavaDemo/src/com/service/ProductServiceImpl.java
@@ -1,31 +1,73 @@
 package com.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.model.Product;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class ProductServiceImpl implements ProductService{
-	
-	static List<Product> plist = new ArrayList<>();
 
 	@Override
 	public void addProduct(Product product) {
-		plist.add(product);
-		System.out.println("======== Added Sucess ===== size ======" + plist.size());
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/productdetail", "root", "");
+			String sql = "insert into productdetail(name,company,price)values('"+product.getName()+"','"+product.getCompany()+"','"+product.getPrice()+"')";
+			Statement stm = con.createStatement();
+			stm.execute(sql);
+			con.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 	}
 
 	@Override
 	public void deleteProduct(int index) {
-		plist.remove(index);
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/productdetail", "root", "");
+
+			String sql = "delete from productdetail where id = 1";
+			Statement stm = con.createStatement();
+			stm.execute(sql);		
+			System.out.println("delete success");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 	}
 
 	@Override
-	public List<Product> getAllProduct() {
-		return plist;
-	}
+	public void getAllProduct(Product product) {
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/productdetail", "root", "");
 
+			String sql = "select * from productdetail ";
+			Statement stm = con.createStatement();
+			ResultSet rs = stm.executeQuery(sql);
+			
+			while(rs.next()) {
+				System.out.println("Id = " + rs.getInt("id"));
+				System.out.println("Name = " + rs.getString("name"));
+				System.out.println("Company = " + rs.getString("company"));
+				System.out.println("Price = " + rs.getString("price"));
+				System.out.println("===================");
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
 	
-}
+
+
+}	
