208847638c0a101fee59bd77ef958c5d7aaf053f
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-06-15 08:42:43 +0545

    add: ManyToMany method

--- HIBERNATE_DEMO/src/main/java/com/model/MappingTest.java
+++ HIBERNATE_DEMO/src/main/java/com/model/MappingTest.java
@@ -12,6 +12,7 @@ public class MappingTest {
 		
 		oneToOne();
 		oneToMany();
+		manyTomany();
 	}
 	
 	//OneToOne
@@ -70,6 +71,52 @@ public class MappingTest {
 		session.save(p2);
 		
 		emp.setPhoneList(Arrays.asList(p1,p2));
+		
+		session.save(emp);
+		session.getTransaction().commit();
+		session.close();
+	}
+	
+	static void manyTomany() {
+		SessionFactory sf = new Configuration().configure().buildSessionFactory();
+		Session session = sf.openSession();
+		session.beginTransaction();
+		
+		Car c = new Car();
+		c.setColor("Red");
+		c.setCompany("Tesla");
+		c.setPrice(4000000);
+		
+		session.save(c);
+		
+		Employee emp = new Employee();
+		emp.setFname("Himanshu");
+		emp.setLname("Pyakurel");
+		emp.setSalary(10000000);
+		emp.setCar(c);
+		
+		Phone p1=new Phone();
+		p1.setNumber("9032361253");
+		p1.setType("NCELL");
+		p1.setEmployee(emp);
+		session.save(p1);
+		
+		Phone p2=new Phone();
+		p2.setNumber("98356512");
+		p2.setType("NTC");
+		p2.setEmployee(emp);
+		session.save(p2);
+		
+		Department d1=new Department();
+		d1.setName("IT");
+		session.save(d1);
+		
+		Department d2=new Department();
+		d2.setName("Admin");
+		session.save(d2);
+		
+		emp.setPhoneList(Arrays.asList(p1,p2));
+		emp.setDeptList(Arrays.asList(d1,d2));
 		
 		session.save(emp);
 		session.getTransaction().commit();
