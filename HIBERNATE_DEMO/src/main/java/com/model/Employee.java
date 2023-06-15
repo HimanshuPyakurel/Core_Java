2e1ecbac2776e59dbd93165573ef8b05aec38a91
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-06-15 08:23:20 +0545

    feat: One To Many

--- HIBERNATE_DEMO/src/main/java/com/model/Employee.java
+++ HIBERNATE_DEMO/src/main/java/com/model/Employee.java
@@ -1,10 +1,13 @@
 package com.model;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -21,6 +24,17 @@ public class Employee {
 	@JoinColumn(name="carId") //FK
 	private Car car;
 	
+	@OneToMany(mappedBy = "employee")
+	private List<Phone> phoneList;
+	
+	
+	
+	public List<Phone> getPhoneList() {
+		return phoneList;
+	}
+	public void setPhoneList(List<Phone> phoneList) {
+		this.phoneList = phoneList;
+	}
 	public int getId() {
 		return id;
 	}
