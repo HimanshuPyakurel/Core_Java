8091f634cd787bc7a73f0ef99b93f5ee61a1b2e6
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-06-15 08:25:36 +0545

    feat: OneToMany method

--- HIBERNATE_DEMO/src/main/java/com/model/MappingTest.java
+++ HIBERNATE_DEMO/src/main/java/com/model/MappingTest.java
@@ -1,5 +1,7 @@
 package com.model;
 
+import java.util.Arrays;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -9,6 +11,7 @@ public class MappingTest {
 	public static void main(String[] args) {
 		
 		oneToOne();
+		oneToMany();
 	}
 	
 	//OneToOne
@@ -33,6 +36,45 @@ public class MappingTest {
 		session.save(emp);
 		session.getTransaction().commit();
 		session.close();
+	}
+	
+	static void oneToMany() {
+		
+		SessionFactory sf = new Configuration().configure().buildSessionFactory();
+		Session session = sf.openSession();
+		session.beginTransaction();
+		
+		Car c = new Car();
+		c.setColor("Red");
+		c.setCompany("Tesla");
+		c.setPrice(4000000);
+		
+		session.save(c);
+		
+		Employee emp = new Employee();
+		emp.setFname("Himanshu");
+		emp.setLname("Pyakurel");
+		emp.setSalary(10000000);
+		emp.setCar(c);
+		
+		Phone p1=new Phone();
+		p1.setNumber("9032361253");
+		p1.setType("NCELL");
+		p1.setEmployee(emp);
+		session.save(p1);
+		
+		Phone p2=new Phone();
+		p2.setNumber("98356512");
+		p2.setType("NTC");
+		p2.setEmployee(emp);
+		session.save(p2);
+		
+		emp.setPhoneList(Arrays.asList(p1,p2));
+		
+		session.save(emp);
+		session.getTransaction().commit();
+		session.close();
+		
 	}
 
 }
