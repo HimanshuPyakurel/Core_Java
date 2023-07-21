0ddf1bf850d596ad6935e98c8361d48aea23d65c
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-07-21 21:46:41 +0545

    Recursion Example

---
+++ Core_Java/src/practice/Recursion.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class Recursion {
+ 
+	static int count=0;  
+	static void p(){  
+	count++;  
+	if(count<=5){  
+	System.out.println("hello "+count);  
+	p();  
+	}  
+	}  
+	public static void main(String[] args) {  
+	p();  
+	}  
+}
