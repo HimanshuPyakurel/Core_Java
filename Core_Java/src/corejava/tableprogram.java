f553c6139489fe95bf220a909bc2d569be31751b
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-08-05 17:48:06 +0545

    tableprogram.java

--- Core_Java/src/corejava/tableprogram.java
+++ Core_Java/src/corejava/tableprogram.java
@@ -5,21 +5,21 @@ public class tableprogram {
 	public static void main(String[] args) {
 		
 		System.out.println("----------------------------------");
-		System.out.print("|  S.No \s");
-		System.out.print("|  Name \s\s\s\s\s");
-		System.out.print("| Phone \s\s\s");
+		System.out.print("|  S.No ");
+		System.out.print("|  Name ");
+		System.out.print("| Phone ");
 		System.out.println("|");
 		System.out.println("----------------------------------");
 		
-		System.out.print("|  1. \s\s\s");
-		System.out.print("|  Himanshu \s");
-		System.out.print("|  984562 \s");
+		System.out.print("|  1. ");
+		System.out.print("|  Himanshu ");
+		System.out.print("|  984562 ");
 		System.out.println("|");
 		System.out.println("----------------------------------");
 		
-		System.out.print("|  2. \s\s\s");
-		System.out.print("|  Pyakurel \s");
-		System.out.print("|  982671 \s");
+		System.out.print("|  2. ");
+		System.out.print("|  Pyakurel ");
+		System.out.print("|  982671 ");
 		System.out.println("|");
 		System.out.println("----------------------------------");
 	}
