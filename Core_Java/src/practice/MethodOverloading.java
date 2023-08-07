4f89e755e5c6dc1fb2a4bcfb580e6ad5c298f779
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-08-07 20:14:50 +0545

    MethodOverloading.java

---
+++ Core_Java/src/practice/MethodOverloading.java
@@ -0,0 +1,22 @@
+package practice;
+
+public class MethodOverloading {
+	// WAP to show method overloading   
+	    void printSum(int a, int b){
+	        int  sum = a + b;
+	        System.out.println(" sum is : " + sum);
+	    }
+
+	    void printSum(String a, String b){
+	        
+	        System.out.println("the total sum is : " + a + " " + b);
+	    }
+	    public static void main(String[] args) {
+	        MethodOverloading m =  new MethodOverloading();
+	        System.out.println("@@@@@@@@@@@@@@@@@@@@@2222");
+	        m.printSum(20, 30);
+	        m.printSum("Himanshu", "Pyakurel");
+	        
+
+	    }
+	}
